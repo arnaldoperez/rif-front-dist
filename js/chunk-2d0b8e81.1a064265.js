d208ac9f84b9459e2cc6e35c4b4ba02d471298cb
Arnaldo Perez <arnaldoeperezb@gmail.com>
2019-10-24 09:57:05 -0400

    Datatable con Vuetify2. Formularios de marcas y Familia

---
+++ js/chunk-2d0b8e81.1a064265.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b8e81"],{"314a":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},c=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("Familias")])])}],l=t("2877"),r={},u=Object(l["a"])(r,a,c,!1,null,null,null);n["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d0b8e81.1a064265.js.map
